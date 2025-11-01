b458c39950c9292e37375c85475526a44ce20bb5
anggamys <anggayunus139@gmail.com>
2025-11-02 02:47:31 +0700

    refactor(docker): simplify Dockerfile and entrypoint script by removing unnecessary migration steps

--- be-camp-cuss/docker-entrypoint.sh
+++ be-camp-cuss/docker-entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 set -e
-echo "Memulai skrip entrypoint Docker..."
 
-echo "Menhapus folder migrasi lama (jika ada)..."
-rm -rf prisma/migrations
-
-echo "Menjalankan migrasi Prisma (tanpa menghapus data)..."
+echo "Menjalankan migrasi Prisma..."
 npx prisma migrate deploy
 
-echo "Menjalankan Prisma generate (pastikan client up-to-date)..."
+echo "Menjalankan Prisma generate..."
 npx prisma generate --schema=./prisma/schema.prisma
 
 echo "Menjalankan aplikasi..."
